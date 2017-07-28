f13d4b3f219f4d8dc9326aee213dfabb46609221
Joel <joel@Joels-MacBook-Air-2.local>
2017-07-28 09:26:10 -0400

    dev

---
+++ wp-content/themes/eurokera/page-templates/about.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: About
+*/
+get_header(); ?>
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+
+<?php get_template_part( 'template-parts/featured-image-parallax' ); ?>
+
+
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+
+<?php get_footer(); ?>
