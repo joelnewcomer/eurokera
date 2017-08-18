ce46619ed22b19f29a65fa0524f58d99bd81d9a4
Joel <joel@Joels-MacBook-Air-2.local>
2017-08-18 08:51:03 -0400

    dev

--- wp-content/themes/eurokera/page-templates/design-gallery.php
+++ wp-content/themes/eurokera/page-templates/design-gallery.php
@@ -9,29 +9,24 @@ get_header(); ?>
 <?php do_action( 'foundationpress_before_content' ); ?>
 
 <section class="users-gallery design-gallery text-center">
-		<div class="btn-filter-wrap">
-			<button class="btn-filter btn-active" data-filter="*">All</button>
-			<button class="btn-filter" data-filter=".lcd">LCD</button>
-			<button class="btn-filter" data-filter=".grooves">Grooves</button>
-			<button class="btn-filter" data-filter=".wok">Wok</button>
-			<button class="btn-filter" data-filter=".holes">Holes</button>
-			<button class="btn-filter" data-filter=".shapes">Shapes</button>
-			<button class="btn-filter" data-filter=".touch-sliders">Touch Sliders</button>
-			<button class="btn-filter" data-filter=".bevels">Bevels</button>
-			<button class="btn-filter" data-filter=".decorations">Decorations</button>
-			<button class="btn-filter" data-filter=".enamel-colors">Enamel Colors</button>
-		</div>
-		<?php 
-		$images = get_field('gallery');
-		$gallery_dot_width = (100 / count($images));
-		?>
-		<style>
-			.owl-theme.users-gallery-carousel .owl-dots .owl-dot {
-				width: <?php echo $gallery_dot_width; ?>%;
-			}
-		</style>
+	<div class="row">
+		
+	<div class="btn-filter-wrap">
+		<button class="btn-filter btn-active" data-filter="*">All</button>
+		<button class="btn-filter" data-filter=".lcd">LCD</button>
+		<button class="btn-filter" data-filter=".grooves">Grooves</button>
+		<button class="btn-filter" data-filter=".wok">Wok</button>
+		<button class="btn-filter" data-filter=".holes">Holes</button>
+		<button class="btn-filter" data-filter=".shapes">Shapes</button>
+		<button class="btn-filter" data-filter=".touch-sliders">Touch Sliders</button>
+		<button class="btn-filter" data-filter=".bevels">Bevels</button>
+		<button class="btn-filter" data-filter=".decorations">Decorations</button>
+		<button class="btn-filter" data-filter=".enamel-colors">Enamel Colors</button>
+	</div>
 	
-	<div class="users-gallery-carousel owl-theme owl-carousel" data-featherlight-gallery data-featherlight-filter="a">
+	<?php $images = get_field('gallery'); ?>
+	
+	<div class="users-gallery" data-featherlight-gallery data-featherlight-filter="a">
 		<?php
 		if( $images ): ?>
 			<?php foreach( $images as $image ): ?>
@@ -42,68 +37,65 @@ get_header(); ?>
 					$cat_classes .= ' ' . sanitize_title($category);
 				}
 				$full_size_url = wp_get_attachment_image_src( $image['ID'], 'full' );
+				$cropped_url = wp_get_attachment_image_src( $image['ID'], 'width=936&height=475&crop=1' ) 
 				?>
-			    <div class="item users-gallery-image<?php echo $cat_classes; ?>">
-				    <a class="gallery" href="<?php echo $full_size_url[0]; ?>">
-			    		<?php echo wp_get_attachment_image( $image['ID'], 'width=936&height=475&crop=1' ) ?>
-				    </a>
-			        <p class="caption"><?php echo $image['caption']; ?></p>
-			    </div>
+				<a class="gallery<?php echo $cat_classes; ?>" href="<?php echo $full_size_url[0]; ?>">
+					<div class="hero" style="background-image: url(<?php echo $cropped_url[0]; ?>);"></div>
+				</a>
+			        <!-- <p class="caption"><?php echo $image['caption']; ?></p> -->
 			<?php endforeach; ?>
 		<?php endif; ?>
 	</div>
+	
 	<script>
 		jQuery(window).load(function(){
-			var owl = jQuery('.owl-carousel').owlCarousel({
-				loop: true,
-				margin: 0,
-				nav: false,
-				dots: true,
-				center: true,
-				autoplay: true,
-			    responsive:{
-			        0:{
-			            items:1
-			        },
-			        600:{
-			            items:2
-			        },
-			        1000:{
-			            items:2
-			        }
-			    }
-			});
-
-			/* animate filter */
-			var owlAnimateFilter = function(even) {
-				jQuery(this)
-				.addClass('__loading')
-				.delay(70 * jQuery(this).parent().index())
-				.queue(function() {
-					jQuery(this).dequeue().removeClass('__loading')
-				})
-			}
-			jQuery('.btn-filter-wrap').on('click', '.btn-filter', function(e) {
-				var filter_data = jQuery(this).data('filter');			
-				/* return if current */
-				if(jQuery(this).hasClass('btn-active')) return;
-				/* active current */
-				jQuery(this).addClass('btn-active').siblings().removeClass('btn-active');
-				/* Filter */
-				owl.owlFilter(filter_data, function(_owl) { 
-					jQuery(_owl).find('.item').each(owlAnimateFilter); 
-				});
-			}); 			
-
 			jQuery.featherlightGallery.prototype.afterContent = function() {
 				var caption = this.$currentTarget.closest('.caption').html();
 				this.$instance.find('.caption').remove();
 				jQuery('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
 			};
-				
-		});		
+		});
+		jQuery('.btn-filter').on( "click", function() {
+			var galleryFilter = jQuery(this).data('filter');
+			// All
+			if (galleryFilter == '*') {
+				jQuery('a.gallery').fadeIn('fast', function() {
+					resetClasses();
+				});
+			} else {
+				jQuery('a.gallery:not(' + galleryFilter + ')').fadeOut( "fast", function() {
+					jQuery('a.gallery' + galleryFilter).fadeIn('fast', function() {
+						resetClasses();
+					});
+				});
+			}
+		});
 	</script>
+	</div>
 </section>
+
+<script>
+function resetClasses() {
+	var loopCounter = 1;
+	jQuery('a.gallery').removeClass('first-item fourth-item fifth-item');
+	jQuery('a.gallery:visible').each(function (i) {
+		if (loopCounter == 1) {
+			jQuery(this).addClass('first-item');
+		}
+		if (loopCounter == 4) {
+			jQuery(this).addClass('fourth-item');
+		}
+		if (loopCounter == 5) {
+			jQuery(this).addClass('fifth-item');
+		}			
+	    loopCounter++;
+	    if (loopCounter == 6) {
+		    loopCounter = 1;
+	    }
+	});
+}
+resetClasses();
+</script>
 
 <?php do_action( 'foundationpress_after_content' ); ?>
 
