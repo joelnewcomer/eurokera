662096b1963c0fb64fa88877294dd434548e2c48
Joel <joel@Joels-MacBook-Air-2.local>
2017-12-07 16:20:14 -0500

    tweak

--- wp-content/themes/eurokera/page-templates/front.php
+++ wp-content/themes/eurokera/page-templates/front.php
@@ -24,26 +24,27 @@ get_header(); ?>
    </div>
 
    <div class="down-arrow bounce animated"><?php get_template_part('assets/images/acc', 'arrow.svg'); ?></div>
+   <script>
+      jQuery( document ).ready(function() {
+      	var nav = jQuery('.header-wrapper').offset();
+      	var $window = jQuery(window);
+   
+      	$window.scroll(function () {
+      	    if ($window.scrollTop() >= nav.top) {
+      	        jQuery(".header-wrapper").addClass("stuck");
+      	    } else {
+      		    jQuery(".header-wrapper").removeClass("stuck");
+      	    }
+      	});
+   
+      	jQuery('.home-banner .down-arrow').click(function() {
+      	    jQuery('html, body').animate({ scrollTop: jQuery('#masthead').offset().top}, 1000);
+      	});
+      });			
+   </script>
 </section>
 
-<script>
-   jQuery( document ).ready(function() {
-   	var nav = jQuery('.header-wrapper').offset();
-   	var $window = jQuery(window);
 
-   	$window.scroll(function () {
-   	    if ($window.scrollTop() >= nav.top) {
-   	        jQuery(".header-wrapper").addClass("stuck");
-   	    } else {
-   		    jQuery(".header-wrapper").removeClass("stuck");
-   	    }
-   	});
-
-   	jQuery('.home-banner .down-arrow').click(function() {
-   	    jQuery('html, body').animate({ scrollTop: jQuery('#masthead').offset().top}, 1000);
-   	});
-   });			
-</script>
 
 <?php $content = get_field('homepage_content'); ?>
 <?php if ($content != '') : ?>
