2f82226046ad747a7bf00edb956b7a47d191b1cf
Joel Newcomer <joel@drumcreative.com>
2018-05-16 08:39:57 -0400

    Contact Page

--- wp-content/themes/eurokera/page-templates/page-contact.php
+++ wp-content/themes/eurokera/page-templates/page-contact.php
@@ -25,6 +25,96 @@ get_header(); ?>
 		</article>
 	<?php endwhile;?>
 	<?php do_action( 'foundationpress_after_content' ); ?>
+
+<section class="contact-locations">
+	<div class="row">
+		<div class="large-12 columns text-center">
+			<h2><?php _e('Our Locations'); ?></h2>
+			
+			<div class="location">
+				<div class="icon-container">
+					<div style="display:table;width:100%;height:100%;">
+						<div style="display:table-cell;vertical-align:middle;">
+							<div style="text-align:center;">
+								<?php get_template_part('assets/images/france.svg'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<h3><?php _e('EuroKera S.N.C.'); ?></h3>
+				<p><?php _e('1 Avenue du Général de Gaulle,'); ?>
+				<?php _e('1 Avenue du Général de Gaulle,'); ?><br />
+				<?php _e('02400 Chierry, France'); ?></p>
+				<div class="button small"><a href="https://www.google.com/maps/place/EuroKera/@49.0404694,3.4142716,17z/data=!4m8!1m2!2m1!1s1+Avenue+du+G%C3%A9n%C3%A9ral+de+Gaulle,%091+Avenue+du+G%C3%A9n%C3%A9ral+de+Gaulle,+02400+Chierry,+France!3m4!1s0x47e8e648f63a46bd:0x995490721e9c1e88!8m2!3d49.0409721!4d3.418197" target="_blank">Map</a></div>
+			</div>
+		
+			<div class="location">
+				<div class="icon-container">
+					<div style="display:table;width:100%;height:100%;">
+						<div style="display:table-cell;vertical-align:middle;">
+							<div style="text-align:center;">
+								<?php get_template_part('assets/images/keraglass.svg'); ?>
+							</div>
+						</div>
+					</div>								
+				</div>
+				<h3><?php _e('KeraGlass'); ?></h3>
+				<p><?php _e('KeraGlass Rue Saint-Laurent, 77167'); ?><br />
+				<?php _e('Bagneaux-sur-Loing, France'); ?></p>
+				<div class="button small"><a href="https://www.google.com/maps/place/Keraglass/@48.229388,2.7031996,17z/data=!3m1!4b1!4m5!3m4!1s0x47e588f317c5ffad:0x18733bcfa0e80251!8m2!3d48.2293844!4d2.7053936" target="_blank">Map</a></div>
+			</div>
+			
+			<div class="location">
+				<div class="icon-container">
+					<div style="display:table;width:100%;height:100%;">
+						<div style="display:table-cell;vertical-align:middle;">
+							<div style="text-align:center;">
+								<?php get_template_part('assets/images/usa.svg'); ?>
+							</div>
+						</div>
+					</div>								
+				</div>
+				<h3><?php _e('EuroKera North America Inc.'); ?></h3>
+				<p><?php _e('140 Southchase Boulevard,'); ?><br />
+				<?php _e('Fountain Inn, SC 29644-8082'); ?></p>
+				<div class="button small"><a href="https://www.google.com/maps/place/140+Southchase+Blvd,+Fountain+Inn,+SC+29644/@34.6928534,-82.241657,17z/data=!3m1!4b1!4m5!3m4!1s0x885820c880d3f8d1:0x7fcc3c63e7e8ee80!8m2!3d34.692849!4d-82.239463" target="_blank">Map</a></div>
+			</div>
+			
+			<div class="location">
+				<div class="icon-container">
+					<div style="display:table;width:100%;height:100%;">
+						<div style="display:table-cell;vertical-align:middle;">
+							<div style="text-align:center;">
+								<?php get_template_part('assets/images/china.svg'); ?>
+							</div>
+						</div>
+					</div>								
+				</div>
+				<h3><?php _e('EuroKera China'); ?></h3>
+				<p><?php _e('Building 11, American Industrial Park,'); ?><br />
+				<?php _e('48 Hongmian Road, Xinhua Town Huadu District, Guangzhou 510800'); ?></p>
+				<div class="button small"><a href="https://www.google.com/maps/place/48+Hong+Mian+Da+Dao+Bei,+Huadu+Qu,+Guangzhou+Shi,+Guangdong+Sheng,+China/@23.408007,113.1686408,17z/data=!4m5!3m4!1s0x3402e8950732003d:0x9e7bcf4a5c37924f!8m2!3d23.408006!4d113.170818" target="_blank">Map</a></div>
+			</div>
+			
+			<div class="location">
+				<div class="icon-container">
+					<div style="display:table;width:100%;height:100%;">
+						<div style="display:table-cell;vertical-align:middle;">
+							<div style="text-align:center;">
+								<?php get_template_part('assets/images/thailand.svg'); ?>
+							</div>
+						</div>
+					</div>								
+				</div>
+				<h3><?php _e('EuroKera Thailand'); ?></h3>
+				<p><?php _e('Hemaraj Eastern Seaboard No.500/61'); ?><br />
+				<?php _e('Moo3, Tambol Tasit, Amphur P, Luakdaeng Rayong 21140'); ?></p>
+				<div class="button small"><a href="https://www.google.com/maps/search/Hemaraj+Eastern+Seaboard+No.500%2F61+Moo+3,+Tambol+Tasit,+Amphur+PLuakdaeng+Rayong+21140/@12.9587766,101.1035924,11z/data=!3m1!4b1" target="_blank">Map</a></div>
+			</div>
+		</div>
+	</div>	
+</section>	<!-- locations -->
+	
 </div> <!-- #page -->
 
 			        		<script>
