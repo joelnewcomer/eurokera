728d71f9f0eb3fb15fda13d7fa6784be6681e1a4
Joel Newcomer <joel@drumcreative.com>
2018-07-30 16:11:15 -0400

    bug fix

--- wp-content/themes/eurokera/template-parts/content-site-links.php
+++ wp-content/themes/eurokera/template-parts/content-site-links.php
@@ -4,7 +4,7 @@
 		<a href="<?php echo get_permalink($manufact_page); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
 	</div>
 	<div class="button blue">
-		<?php $users_page = get_page_by_path('cooktop-manufacturers'); ?>
+		<?php $users_page = get_page_by_path('cooktop-users'); ?>
 		<a href="<?php echo get_permalink($users_page); ?>"><?php _e('Cooktop Users'); ?></a>
 	</div>
 </div>
