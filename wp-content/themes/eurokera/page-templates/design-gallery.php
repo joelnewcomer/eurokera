fd3ddae9bb739ebd0193747d427d38eb7e98a472
Joel Newcomer <joel@drumcreative.com>
2018-09-27 14:37:01 -0400

    Text change on enamel gallery

--- wp-content/themes/eurokera/page-templates/design-gallery.php
+++ wp-content/themes/eurokera/page-templates/design-gallery.php
@@ -76,7 +76,7 @@ get_header(); ?>
 		<h2><?php _e('Enamel Color Samples'); ?></h2>
 		<?php $contact_page = get_page_by_path('contact'); ?>
 		<?php $contact_url = get_permalink($contact_page->ID); ?>
-		<?php $contact_string = sprintf( __('The color samples below are digital renderings with for only intend to illustrate the possibilities in terms of decoration offered by EuroKera. For any enquiry, we recommend to <a href="%s">contact EuroKera</a>.', 'foundationpress'), $contact_url); ?>
+		<?php $contact_string = sprintf( __('The color samples below are digital renderings and are only intended to illustrate the decoration possibilities. Please <a href="%s">contact EuroKera</a> for further questions.', 'foundationpress'), $contact_url); ?>
 		<p class="small"><?php echo $contact_string; ?></p>
 
 		<?php
