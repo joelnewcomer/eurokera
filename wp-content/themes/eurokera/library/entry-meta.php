965f89744e5fd89332b92221c2efa295ebb33f8a
Joel Newcomer <joel@drumcreative.com>
2018-10-02 10:01:58 -0400

    Update post meta

--- wp-content/themes/eurokera/library/entry-meta.php
+++ wp-content/themes/eurokera/library/entry-meta.php
@@ -10,32 +10,7 @@ if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function drum_entry_meta() {
 		global $post;
 		echo '<ul class="post-meta group">';
-			echo '<li>';
-			// Get categories but exclude Uncategorized
-			$cats = wp_get_post_categories($post->ID, array('exclude' => 1));
-			$num_cats = count($cats);
-			$counter = 1;
-			foreach ($cats as $cat) {
-				echo '<a href="' . get_category_link($cat) . '">' . get_cat_name($cat) . '</a>';
-				if ($num_cats > 1 && $counter < $num_cats) {
-					echo ' / ';
-				}
-				$counter++;
-			}
-			echo '</li>';
-			echo '<li>';
-			get_template_part("assets/images/clock-o.svg");
-			echo '<time class="updated" datetime="'. get_the_time( 'c' ) .'">'. sprintf( __( '%s', 'foundationpress' ), get_the_date() ) .'</time></li>';
-			echo '<li><div class="byline"><span>By: ' . get_the_author() . '</div></li>';
-			if ( get_comments_number() > 0 ) : ?>
-				<li>
-				<?php if (is_single()) : ?>
-					<a href="#comments"><?php get_template_part('assets/images/comment.svg'); ?><?php echo get_comments_number(); ?></a>
-				<?php else : ?>
-					<a href="<?php the_permalink(); ?>#comments"><?php get_template_part('assets/images/comment.svg'); ?><?php echo get_comments_number(); ?></a>
-				<?php endif; ?>
-				</li>
-			<?php endif; ?>
+			echo '<li><div class="byline"><span>By: ' . get_the_author() . '</div></li>'; ?>
 		</ul> <!-- post-meta -->
 	<?php }
 endif; ?>
