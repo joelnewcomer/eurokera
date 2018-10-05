0e12a7be4ce1252e41b66130001f69a3d1e9a12c
Joel Newcomer <joel@drumcreative.com>
2018-10-05 09:14:40 -0400

    Add Author field to blog posts

--- wp-content/themes/eurokera/library/entry-meta.php
+++ wp-content/themes/eurokera/library/entry-meta.php
@@ -9,8 +9,12 @@
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function drum_entry_meta() {
 		global $post;
+		$author = get_field('author');
+		if ($author == '') {
+			$author = get_the_author();
+		}
 		echo '<ul class="post-meta group">';
-			echo '<li><div class="byline"><span>By: ' . get_the_author() . '</div></li>'; ?>
+			echo '<li><div class="byline"><span>By: ' . $author . '</div></li>'; ?>
 		</ul> <!-- post-meta -->
 	<?php }
 endif; ?>
