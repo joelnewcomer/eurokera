b2cc70ef6e72ab5964c045545f5815208d4d34c9
Joel Newcomer <joel@drumcreative.com>
2018-11-07 15:24:03 -0500

    WPML work-around

--- wp-content/themes/eurokera/template-parts/content-site-links.php
+++ wp-content/themes/eurokera/template-parts/content-site-links.php
@@ -1,10 +1,13 @@
 <div class="site-links">
 	<div class="button">
-		<?php $manufact_page = get_page_by_path('cooktop-manufacturers'); ?>
-		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($manufact_page)); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
+		<?php $manufact_page = get_page_by_path('cooktop-manufacturers');
+		$icl_manufact_page_id = icl_object_id($manufact_page->ID, 'page', true);
+		?>
+		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($icl_manufact_page_id)); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
 	</div>
 	<div class="button blue">
-		<?php $users_page = get_page_by_path('cooktop-users'); ?>
-		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($users_page)); ?>"><?php _e('Cooktop Users'); ?></a>
+		<?php $users_page = get_page_by_path('cooktop-users');
+		$icl_users_page_id = icl_object_id($users_page->ID, 'page', true); ?>
+		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($icl_users_page_id)); ?>"><?php _e('Cooktop Users'); ?></a>
 	</div>
 </div>
