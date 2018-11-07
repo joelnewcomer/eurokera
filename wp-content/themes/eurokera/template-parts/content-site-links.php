4bb37fb173fd0b50addd721fc15bca7a1053995f
Joel Newcomer <joel@drumcreative.com>
2018-11-07 15:19:28 -0500

    WPML work-around

--- wp-content/themes/eurokera/template-parts/content-site-links.php
+++ wp-content/themes/eurokera/template-parts/content-site-links.php
@@ -1,10 +1,10 @@
 <div class="site-links">
 	<div class="button">
 		<?php $manufact_page = get_page_by_path('cooktop-manufacturers'); ?>
-		<a href="<?php echo get_permalink($manufact_page); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
+		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($manufact_page)); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
 	</div>
 	<div class="button blue">
 		<?php $users_page = get_page_by_path('cooktop-users'); ?>
-		<a href="<?php echo get_permalink($users_page); ?>"><?php _e('Cooktop Users'); ?></a>
+		<a href="<?php echo apply_filters( 'wpml_permalink', get_permalink($users_page)); ?>"><?php _e('Cooktop Users'); ?></a>
 	</div>
 </div>
