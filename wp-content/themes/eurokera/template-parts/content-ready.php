8e2d8e73fac83851e59e96e9c4048544271da773
Joel Newcomer <joel@drumcreative.com>
2018-11-08 08:48:33 -0500

    Tweaks

--- wp-content/themes/eurokera/template-parts/content-ready.php
+++ wp-content/themes/eurokera/template-parts/content-ready.php
@@ -2,8 +2,10 @@
 	<div class="row">
 		<div class="large-12 columns text-center">
 			<h2><?php _e('Ready To Build An Innovative Cooktop?','foundationpress'); ?></h2>
-			<?php $contact_page = get_page_by_path('contact'); ?>
-			<div class="button reverse"><a href="<?php echo get_permalink($contact_page->ID); ?>" id="work-together-btn"><?php _e('Let\'s Work Together','foundationpress'); ?></a></div>
+			<?php $contact_page = get_page_by_path('contact');
+			$icl_contact_page_id = icl_object_id($contact_page->ID, 'page', true);	
+			?>
+			<div class="button reverse"><a href="<?php echo get_permalink($icl_contact_page_id); ?>" id="work-together-btn"><?php _e('Let\'s Work Together','foundationpress'); ?></a></div>
 		</div>
 	</div>
 </section>
