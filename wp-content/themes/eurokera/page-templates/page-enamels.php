115689a3e7d4b5baa02807d35b5294e47d58b391
Joel Newcomer <joel@drumcreative.com>
2018-12-07 10:23:31 -0500

    tweak

--- wp-content/themes/eurokera/page-templates/page-enamels.php
+++ wp-content/themes/eurokera/page-templates/page-enamels.php
@@ -156,7 +156,7 @@ if (typeof bxSlider === "function") {
 	jQuery(window).load(function(){
     	var slider = jQuery('.bxslider').bxSlider({
     	    auto: false,
-    	    pager: true,
+    	    pager: (jQuery(".bxslider").length > 1) ? true: false,
     	    controls: false,
     	    mode: 'fade',
     	    speed: 1000,
