30c56be40721f31012d8efbff66a86a2d7dad49d
Joel Newcomer <joel@drumcreative.com>
2018-12-12 15:51:25 -0500

    grrr

--- wp-content/themes/eurokera/assets/javascript/footer-scripts.js
+++ wp-content/themes/eurokera/assets/javascript/footer-scripts.js
@@ -184,10 +184,6 @@ jQuery.fn.owlFilter = function(data, callback) {
  */
 jQuery(document).ready(function($){var offset=300,offset_opacity=1200,scroll_top_duration=700,$back_to_top=jQuery('.cd-top');jQuery(window).scroll(function(){(jQuery(this).scrollTop()>offset)?$back_to_top.addClass('cd-is-visible'):$back_to_top.removeClass('cd-is-visible cd-fade-out');if(jQuery(this).scrollTop()>offset_opacity){$back_to_top.addClass('cd-fade-out')}});$back_to_top.on('click',function(event){event.preventDefault();jQuery('body,html').animate({scrollTop:0,},scroll_top_duration)})});
 
-/* Lazy Load XT 1.1.0 | MIT License */
-!function(a){var b=a.lazyLoadXT,c=b.bgAttr||"data-bg";b.selector+=",["+c+"]",a(document).on("lazyshow",function(b){var d=a(b.target),e=d.attr(c);e&&d.css("background-image","url('"+e+"')").removeAttr(c).triggerHandler("load")})}(window.jQuery||window.Zepto||window.$);
-
-
 /**
 * Initialize jQuery Plugins
 **/
