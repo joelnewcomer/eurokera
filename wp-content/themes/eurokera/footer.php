1a6fd071612bfc1788999e5d4d71e4c16885d294
Joel Newcomer <joel@drumcreative.com>
2018-12-19 11:44:18 -0500

    LinkedIn Tracking

--- wp-content/themes/eurokera/footer.php
+++ wp-content/themes/eurokera/footer.php
@@ -135,5 +135,21 @@ foreach ($langCodes as $langCode) {
 <?php wp_footer(); ?>
 
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
+
+<script type="text/javascript">
+_linkedin_partner_id = "576260";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script><script type="text/javascript">
+(function(){var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js<https://snap.licdn.com/li.lms-analytics/insight.min.js>";
+s.parentNode.insertBefore(b, s);})();
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=576260&fmt=gif<https://dc.ads.linkedin.com/collect/?pid=576260&fmt=gif>" />
+</noscript>
+
 </body>
 </html>
