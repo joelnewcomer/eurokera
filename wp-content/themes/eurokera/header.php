b7d40193a94f295b813735be5271ce62c4da3350
Joel Newcomer <joel@drumcreative.com>
2018-12-19 13:16:00 -0500

    remove animsition

--- wp-content/themes/eurokera/header.php
+++ wp-content/themes/eurokera/header.php
@@ -67,7 +67,7 @@
 	<?php if(!preg_match('/(?i)msie [5-9]/',$_SERVER['HTTP_USER_AGENT'])) : ?>
 		<div class="animsition-shell">
 		<script>
-			jQuery('.animsition-shell').addClass( 'animsition' );
+			// jQuery('.animsition-shell').addClass( 'animsition' );
 		</script>
 	<?php endif; ?>	
 	
