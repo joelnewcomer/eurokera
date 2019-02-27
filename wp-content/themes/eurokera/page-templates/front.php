5b1361478ce994c087a4685d19ecc30560e3a708
Joel Newcomer <joel@drumcreative.com>
2019-02-27 10:12:10 -0500

    grrr

--- wp-content/themes/eurokera/page-templates/front.php
+++ wp-content/themes/eurokera/page-templates/front.php
@@ -131,15 +131,19 @@ get_header(); ?>
 	?>
 <?php if (ICL_LANGUAGE_CODE == 'zh-hans') : ?>	
 	<?php
-	$video_markup = '<a class="home-video vp-s vp-mp4-type" href="' . get_field('video_url') . '" data-autoplay="1" data-dwrap="1" style="background-image: url(' . $video_poster . ');"><div class="row"><div class="large-12 columns text-center">';
+	$video_markup = '<a class="home-video vp-s vp-mp4-type" href="' . get_field('video_url') . '" data-autoplay="1" data-dwrap="1" style="background-image: url(' . $video_poster . ');"></a>';
+	echo apply_filters('the_content', $video_markup);
+	$video_inner = '<div class="row"><div class="large-12 columns text-center">';
 	ob_start();
 	get_template_part('assets/images/play', 'button.svg');
 	$play_button = ob_get_contents();
 	ob_end_clean();
-	$video_markup .= $play_button;
-	$video_markup .= '<br />' . get_field('video_title') . '</div></div></a>';
-	echo apply_filters('the_content', $video_markup);
+	$video_inner .= $play_button;
+	$video_inner .= '<br />' . get_field('video_title') . '</div></div>';
 	?>
+	<script>
+		jQuery('a.home-video').html('<?php echo $video_inner; ?>');
+	</script>
 <?php else: ?>
 	<a class="home-video" href="<?php echo get_field('video_url'); ?>?autoplay=1&modestbranding=1&showinfo=0&rel=0" data-featherlight="iframe" data-featherlight-iframe-width="960" data-featherlight-iframe-height="540" style="background-image: url(<?php echo $video_poster; ?>);">
 		<div class="row">
