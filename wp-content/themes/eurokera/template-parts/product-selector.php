5246264d45897b1963a173e6c600d8e3f36fd474
Joel Newcomer <joel@drumcreative.com>
2019-04-18 12:00:18 -0400

    grrr

--- wp-content/themes/eurokera/template-parts/product-selector.php
+++ wp-content/themes/eurokera/template-parts/product-selector.php
@@ -147,7 +147,7 @@
 					
 
 						<?php $pro_cooking_page = get_page_by_path('commercial/cooking');
-						$icl_pro_cooking_page_id = icl_object_id($enamels_page->ID, 'page', true);
+						$icl_pro_cooking_page_id = icl_object_id($pro_cooking_page->ID, 'page', true);
 						$pro_cooking_url = get_permalink($icl_pro_cooking_page_id); ?>	
 						
 						<a class="large-4 medium-4 small-6 columns text-center product-selector-product always-show" href="<?php echo $pro_cooking_url; ?>">
