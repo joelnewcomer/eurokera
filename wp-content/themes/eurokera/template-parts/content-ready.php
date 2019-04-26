d60db303ee690682ac5c9fac18e96e73f5d2cd45
Joel Newcomer <joel@drumcreative.com>
2019-04-26 09:09:49 -0400

    dev

--- wp-content/themes/eurokera/template-parts/content-ready.php
+++ wp-content/themes/eurokera/template-parts/content-ready.php
@@ -8,9 +8,25 @@
 			<div class="button reverse"><a href="<?php echo get_permalink($icl_contact_page_id); ?>" id="work-together-btn"><?php _e('Let\'s Work Together','foundationpress'); ?></a></div> -->
 			<h2><?php _e('Let\'s Work Together','foundationpress'); ?></h2>
 			<?php
+			/*
+			Form Options:
+			Home Cooking	
+			Fireplaces
+			Versâtis
+			Specialties
+			Professional Cooking
+			*/
 			$field_values = "";
-			if (is_page_template('page-templates/versatis.php')) {
-				$field_values = " field_values='solution=Versâtis'";
+			$solution = get_sub_field('auto_populate');
+			if ($solution != '') {
+				$field_values = " field_values='solution=" . _e($solution,'foundationpress') . "'";
+			} else {
+				if (is_page_template('page-templates/versatis.php')) {
+					$field_values = " field_values='solution=" . _e('Versâtis','foundationpress') . "'";
+				}
+				if (is_page_template('page-templates/cooking.php')) {
+					$field_values = " field_values='solution=" . _e('Professional Cooking','foundationpress') . "'";
+				}
 			}
 			?>
 			<?php echo do_shortcode('[gravityform id="1" title="false" description="false"' . $field_values . ']'); ?>
