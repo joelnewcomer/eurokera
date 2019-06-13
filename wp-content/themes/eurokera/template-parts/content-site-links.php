5293e6a4c8947c3145b3abff5fa90061a52ca743
Joel Newcomer <joel@drumcreative.com>
2019-06-13 14:38:13 -0400

    fix links

--- wp-content/themes/eurokera/template-parts/content-site-links.php
+++ wp-content/themes/eurokera/template-parts/content-site-links.php
@@ -1,12 +1,12 @@
 <div class="site-links">
 	<div class="button">
-		<?php $manufact_page = get_page_by_path('cooktop-manufacturers');
+		<?php $manufact_page = get_page_by_path('cooking/cooktop-manufacturers');
 		$icl_manufact_page_id = icl_object_id($manufact_page->ID, 'page', true);
 		?>
 		<a href="<?php echo get_permalink($icl_manufact_page_id); ?>"><?php _e('Cooktop Manufacturers'); ?></a>
 	</div>
 	<div class="button blue">
-		<?php $users_page = get_page_by_path('cooktop-users');
+		<?php $users_page = get_page_by_path('cooking/cooktop-users');
 		$icl_users_page_id = icl_object_id($users_page->ID, 'page', true); ?>
 		<a href="<?php echo get_permalink($icl_users_page_id); ?>"><?php _e('Cooktop Users'); ?></a>
 	</div>
