b0ec84de10fd45366425a9b1d14ffe7d6a352950
Joel Newcomer <joel@drumcreative.com>
2019-09-06 11:30:21 -0400

    tweak

--- wp-content/themes/eurokera/assets/images/play-button-simple.svg.php
+++ wp-content/themes/eurokera/assets/images/play-button-simple.svg.php
@@ -1,3 +1,1 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="79.715" height="95.1" viewBox="0 0 79.715 95.1">
-  <path id="Path_53" data-name="Path 53" d="M885.474,8621.734v95.1l79.715-47.781Z" transform="translate(-885.474 -8621.735)" fill="#fff"/>
-</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="79.715" height="95.1" viewBox="0 0 79.715 95.1"><path id="Path_53" data-name="Path 53" d="M885.474,8621.734v95.1l79.715-47.781Z" transform="translate(-885.474 -8621.735)" fill="#fff"/></svg>
